215d71e6236ca7c392091a8aca3e81dc6611f1bd
adil <adilhayat01@gmail.com>
2013-07-28 09:09:52 -0400

    without duplicates allowed

--- src/AdjacencyList.java
+++ src/AdjacencyList.java
@@ -1,23 +1,49 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Iterator;
-import java.io.IOException;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
 
 
 public class AdjacencyList {
 	 int n=200;
+	 int a,b;
+	 int k=0;
 	 List<Integer>[] adj;
-	       	  	
+	 Random generator = new Random();
+	 int vertexCount=n;
+	 int minCutEdgeCount;
+	 
 	public AdjacencyList(String fileName)throws IOException, NumberFormatException{
 
 		 adj = (List<Integer>[])new List[n];
 		 for (int i = 0; i < n; i++)
 		 adj[i] = new ArrayList<Integer>();
 		 readFile(fileName);
-	}
+		 		 
+		 while(vertexCount > 2){
+			 a = generator.nextInt(n);
+			 b = generator.nextInt(n);			 
+			 contractEdge(a, b);
+//			 System.out.println(vertexCount);
+		 }//end while
+		 
+		 while(k<n){
+			 if(adj[k]!=null){
+				 System.out.println(adj[k]);
+				 System.out.println(adj[k].size());
+			 }
+			 k++;
+		 }		 
+		 
+//		 removeSelfLoops(k);
+//		 minCutEdgeCount= countEgde(k);
+//		 System.out.println("Edges in min cut:" + minCutEdgeCount);
+	}// end constructor
 	
 	public void readFile(String fileName) throws IOException, NumberFormatException{
 		FileInputStream in = new FileInputStream(fileName);
@@ -36,7 +62,7 @@ public class AdjacencyList {
 				}
 				String[] strLine = (line.split("\t"));
 				
-				for(int j=0;j<strLine.length;j++){
+				for(int j=1;j<strLine.length;j++){
 					k=Integer.parseInt(strLine[j]);
 					addEdge(i, k);
 				}
@@ -58,7 +84,7 @@ public class AdjacencyList {
         }    
     }// end removeEdge
 	
-	 boolean hasEdge(int i, int j) {
+	 public boolean hasEdge(int i, int j) {
 	       return adj[i].contains(j);
 	 }// end hasEdge
 	 
@@ -66,10 +92,53 @@ public class AdjacencyList {
 	        return adj[i];
 	 }// end outEdge
 	 
-	 List<Integer> inEdges(int i) {
+	 public List<Integer> inEdges(int i) {
 	   	List<Integer> edges = new ArrayList<Integer>();
         for (int j = 0; j < n; j++)
         	if (adj[j].contains(i))    edges.add(j);
 	        return edges;
-	    }	    
+	    }// end inEdge
+	 
+	 public void contractEdge(int i, int j){
+		 if(adj[i] != null && adj[j]!=null){
+			 ListIterator<Integer> it= adj[j].listIterator();
+			 while(it.hasNext()){
+				 int k=it.next();
+				 if(!(adj[i].contains(k))){
+					 addEdge(i, k);
+				 }//end if
+				 if(adj[i].contains(j+1) || adj[i].contains(i+1)){
+					 removeEdge(i, j+1);
+					 removeEdge(i, i+1);
+				 }
+			 }//end while 
+			 adj[j]=null;
+			 vertexCount--;	
+		 }// end outer if		 
+	 } // end contract edge
+	 
+	 public void removeSelfLoops(int i)
+	 {
+		 if(adj[i] != null){
+			 ListIterator<Integer> itr= adj[i].listIterator();
+			 while(itr.hasNext()){
+				 int k=itr.next();
+				 if(k==(i+1)){
+					 removeEdge(i, k);
+					 return;
+				 }//end if 
+			 }//end while
+		 }// end outer if
+	 }// end self loops
+	 
+	 public int countEgde(int i)
+	 {
+		int count;		
+		if(adj[i]==null){
+			count=0;			
+		}else{
+			count=adj[i].size();
+		}
+		return count;
+	 }// end countEdge
 }
--- src/sample.java
+++ src/sample.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class sample {
 
@@ -5,8 +8,20 @@ public class sample {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int a= 5;
-		System.out.println(Math.IEEEremainder(a, 2)!= 0);
-	}
+//		int a= 5;
+//		System.out.println(Math.IEEEremainder(a, 2)!= 0);
+		
+		int array[]= {1,2,3,4,5};
+		
+		List<Integer> ar = new ArrayList<Integer>();
+		for(int element:array){
+			ar.add(element);
+		}
+		
+		ListIterator<Integer> it = ar.listIterator(); 
+		while(it.hasNext()){
+			System.out.printf("%d: %d \n",it.nextIndex(),it.next());
+		}// end while 		
+	}// end main
 
 }
