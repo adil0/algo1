163dfaeaa9ac564ad0cff32fc92fbeac5f12dcaa
adil <adilhayat01@gmail.com>
2013-08-05 01:37:35 -0400

    Assgn 4 final

--- src/scc.java
+++ src/scc.java
@@ -37,6 +37,11 @@ public class scc {
 			finTimes[i]=0;
 			leader[i]=0;
 		}
+		
+		int[] res={1,1,1,1,1};
+		for(int i=0;i<5;i++){
+			System.out.println(res[i]);
+		}
 		readFile(fileName);
 		System.out.println("File Reading Done");
 //		System.out.println(outEdges(1));
@@ -50,23 +55,42 @@ public class scc {
 		for(int k=(n-1);k>0;k--){
 			if(track[k]==0){
 				DFS(k);
-				System.out.println("Rev. Processed for vertex: "+ k);
+//				System.out.println("Rev. Processed for vertex: "+ k);
 			}
 		}
+		System.out.println("Finishing Times Calculated");
 		
+		adjRev=null;
+		System.gc();
 		// create Finishing times adjacency list
 		createFinTimeList();
+		System.out.println("Finishing Times List created");
 		
 		// calculate the master nodes
-		for(int k=(n-1);k>0;k--){
-			if(track[k]==0){
-				sccSz=1;
-				s=k;
-				DFSF(k);
-				System.out.println(sccSz);
-			}
+		System.out.println("Print out the size of SCCs");
+
+		for(int i=1;i<n;i++){
+			track[i]=0;
 		}
 
+		for(int k=(n-1);k>0;k--){
+			if(track[k]==0){
+				sccSz=0;
+				s=k;
+				DFSF(k);
+//				System.out.println(sccSz);
+				for(int i=0;i<5;i++){
+					if(sccSz>res[i]){
+						res[i]=sccSz;
+						break;
+					}
+				}				
+			}
+		}
+	
+		for(int i=0;i<5;i++){
+			System.out.println(res[i]);
+		}
 	}// end constructor
 	
 	public void readFile(String fileName) throws IOException, NumberFormatException{
@@ -119,25 +143,34 @@ public class scc {
 	 public void DFS(int i){
 		track[i]=1;
 //		System.out.println(adjRev[i]);
-		 ListIterator<Integer> it= adjRev[i].listIterator();
+		ListIterator<Integer> it= adjRev[i].listIterator();
 		 
 		 while(it.hasNext()){
 			 int k=it.next();
 //			 System.out.println("k: " + k);
 			 if(track[k]==0){				 
 				 DFS(k);
+			 }else{
+					t++;
+					finTimes[i]=t;
+				 return;
 			 }
 		 }
-		 
 		t++;
-		finTimes[i]=t;
+		finTimes[i]=t; 
 	 }
 	 
 	 public void createFinTimeList(){
-		 for(int i=1;i<n;i++){
-			 ListIterator<Integer> it=adj[i].listIterator();
-			 while(it.hasNext()){				 
-				adjFinTimes[finTimes[i]].add(finTimes[ it.next()]); 
+		 for(int i=(n-1);i>0;i--){
+			 int f=finTimes[i];
+//			 System.out.println("Finishing time of " + i + "=" + f);
+//			 System.out.println(adj[i]);
+			 ListIterator<Integer> itr=adj[i].listIterator();
+			 while(itr.hasNext()){
+				 int k=itr.next();
+				 int p=finTimes[k];
+//				 System.out.println("f="+f+" and p="+p);
+				adjFinTimes[f].add(p); 
 			 }
 		 }
 	 }
@@ -152,6 +185,8 @@ public class scc {
 			 if(track[k]==0){
 				 sccSz++;
 				 DFSF(k);
+			 }else{
+				 return;
 			 }
 		 }
 	 }
