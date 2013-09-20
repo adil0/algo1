c1996e1d977ec2f20e03315778bd35136b453967
adil <adilhayat01@gmail.com>
2013-09-20 03:35:34 -0400

    Assgn2 prob1

--- src/cluster.java
+++ src/cluster.java
@@ -3,7 +3,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 public class cluster {
@@ -11,23 +14,37 @@ public class cluster {
 	 long mc=0;
 	 List<Integer>[] adj;
 	 List<Integer>[] wt;
-	 List<Integer> vst;
-	 List<Integer> nonVst;
+	 HashMap<Integer, String> map = new HashMap<Integer, String>();
+	 Set<Integer> keys;
+	 TreeSet<Integer> sortedKeys;
 	 int reqClust=4;
+	 int[] leader,leaderCount;
+	 int maxSpacing;
 	 
 	public cluster(String fileName) throws NumberFormatException,IOException{
 		readFile(fileName);
 //		System.out.println("n="+n);
 		clustCount=n;
-		
-		System.out.println("adj[1]="+adj[1]);
-		System.out.println("wt[1]="+wt[1]);
-				
-		while(clustCount > reqClust){
-			mergeClusters();
-			clustCount--;
+		leader=new int[n+1];
+		leaderCount=new int[n+1];
+		for(int i=1;i<(n+1);i++){
+			leader[i]=i;
+			leaderCount[i]=1;
 		}
 		
+//		System.out.println("adj[1]="+adj[1]);
+//		System.out.println("wt[1]="+wt[1]);
+//		System.out.println(map.get(6).trim());
+		
+		keys = map.keySet();
+		sortedKeys= new TreeSet<Integer>(keys);
+		
+		maxSpacing=mergeClusters();
+		System.out.println("Max Spacing="+maxSpacing);
+		for(int i=1;i<(n+1);i++){
+//			System.out.printf("Leader[%d]=%d \n",i,leader[i]);
+		}
+
 	}
 
 	public void readFile(String fileName) throws IOException, NumberFormatException{
@@ -36,6 +53,7 @@ public class cluster {
 			
 			int lc=0;
 			int weigh,src,dest;
+			String s;
 			String line = null;
 			while (true)
 			{
@@ -52,12 +70,9 @@ public class cluster {
 					// array list declarations
 					adj = (List<Integer>[])new List[(n+1)];
 					wt = (List<Integer>[])new List[(n+1)];
-					vst = new ArrayList<Integer>();
-					nonVst = new ArrayList<Integer>();
 					for (int i = 1;i < (n+1); i++){
 					adj[i] = new ArrayList<Integer>();
 					wt[i] = new ArrayList<Integer>();
-					nonVst.add(i);
 					}
 				}else{
 					src=Integer.parseInt(strLine[0]);
@@ -67,6 +82,10 @@ public class cluster {
 					addWeight(src,weigh);
 					addEdge(dest, src);
 					addWeight(dest,weigh);
+					s=map.get(weigh);
+					if(s==null){s="";}
+					s = s +" " +String.valueOf(src)+"-"+String.valueOf(dest);
+					map.put(weigh, s);
 				}
 				lc++;
 			}// end while
@@ -80,13 +99,51 @@ public class cluster {
         wt[i].add(j);
     }
 
-	public void mergeClusters(){
-		
-	}
+	public int mergeClusters(){
+		String s;
+		String[] els;
+		String[] el;
+		int source,dest,removedLeader,spacing=0,t=0;
+		for(Integer keys:sortedKeys){
+//			System.out.println("Outer Cluster Count="+clustCount);
+			s=map.get(keys).trim();
+			els= s.split(" ");
+			for(int i=0;i<els.length;i++){
+				el= els[i].split("-");
+//				System.out.println(els[i]);
+//				System.out.println("Cluster Count="+clustCount);
+				source= Integer.parseInt(el[0]);
+				dest=Integer.parseInt(el[1]);
+				if(clustCount == reqClust && leader[source]!=leader[dest]){if(t==0){spacing=keys;t++;}break;}
+				if(leader[source]!=leader[dest]){
+					if(leaderCount[source] >= leaderCount[dest]){
+						removedLeader=leader[dest];
+						for(int k=1;k<(n+1);k++){
+							if(leader[k]==removedLeader){
+								leader[k]=leader[source];
+								leaderCount[source]++;
+							}
+						}
+					}else{
+						removedLeader=leader[source];
+						for(int k=1;k<(n+1);k++){
+							if(leader[k]==removedLeader){
+								leader[k]=leader[dest];
+								leaderCount[dest]++;
+							}
+						}
+					}
+					clustCount--;
+				}// end outer if
+			}// end for			
+		}// end outer for
+		return spacing;
+	} // end function
 	
 	
 	public static void main(String[] args) throws NumberFormatException,IOException {
 		cluster c1 = new cluster("/home/adil/Downloads/clustering1.txt");
+//		cluster c2 = new cluster("/home/adil/Downloads/assgn2test1.txt");
 	}
 
 }
