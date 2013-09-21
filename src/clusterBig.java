b33e9bd4a8f3fa67e28bb06e0cbb5bbaf7107358
adil <adilhayat01@gmail.com>
2013-09-21 09:21:55 -0400

    Assign2prob2

--- src/clusterBig.java
+++ src/clusterBig.java
@@ -8,12 +8,10 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import org.apache.commons.math3.util.ArithmeticUtils;
-import org.apache.commons.math3.util.MathUtils;
 
 
 public class clusterBig {
-	 int n,clustCount,minSep;
+	 int n,clustCount,minSep=3;
 	 List <String>[] group;
 	 ArrayList<String> adj=new ArrayList<String>();
 	 int maxSpacing,codeLen;
@@ -22,16 +20,25 @@ public class clusterBig {
 		readFile(fileName);
 		System.out.println("n="+n);
 		System.out.println("code Length="+codeLen);
-		System.out.println(adj.get(0));
-	}
+		clustCount=n;
+		
+		groupClusters();
+		
+		for(int k=0;k<codeLen;k++){
+			for(int i=0;i<group[k].size();i++){
+//				System.out.println(group[k].get(i));
+			}
+		}
+		
+		findNearestAndMerge();
+		System.out.println("Cluster Count="+clustCount);
+	}// end constructor
 	
 	public void readFile(String fileName) throws IOException, NumberFormatException{
 		FileInputStream in = new FileInputStream(fileName);
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			
 			int lc=0;
-			int weigh,src,dest;
-			String s;
 			String line = null;
 			while (true)
 			{
@@ -57,11 +64,52 @@ public class clusterBig {
 			}// end while
 	}// end readLine
 			
+	public void groupClusters(){
+		int sum;
+		String s;
+		String[] sArray;
+		int el;
+		for(int i=0;i<adj.size();i++){
+			sum=0;
+			el=0;
+			s=adj.get(i);
+			sArray=s.split(" ");
+			for(int k=(codeLen-1);k>=0;k--){
+				el= (int) (el + Integer.parseInt(sArray[k])*Math.pow(10, k));
+			}
+			for(int j=0;j<codeLen;j++){
+				sum= sum + el%10;
+				el=el/10;
+			}// end inner for
+			group[sum].add(adj.get(i));
+		}// end for
+	}// end function
 	
+	public void findNearestAndMerge(){
+		int lookFwd;
+		for(int i=0;i<codeLen;i++){
+			if(group[i].size()==0){
+				System.out.printf("There is no element with sum=%d",i);
+			}else{
+				lookFwd=i+minSep-1;
+				if(lookFwd >= codeLen){
+					break; // get outside loop if 
+							//we have already looked at all elements
+				}
+				clustCount--;
+			}// end else
+		}// end for		
+	}//end function
+	
+	public int findDist(String s1, String s2){
+		int dist=0;
+		
+		return dist;
+	}
 	
 	public static void main(String[] args) throws NumberFormatException,IOException {
-		clusterBig c1 = new clusterBig("/home/adil/Downloads/clustering_big.txt");
-//		cluster c2 = new cluster("/home/adil/Downloads/assgn2test1.txt");
+//		clusterBig c1 = new clusterBig("/home/adil/Downloads/clustering_big.txt");
+		clusterBig c2 = new clusterBig("/home/adil/Downloads/assign2test2.txt");
 	}
 
 }
